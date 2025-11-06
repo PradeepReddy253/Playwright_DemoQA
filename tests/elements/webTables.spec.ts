605c5e7311520f451af3622bd6ff373c399c3f88
Chevula Pradeep Reddy <chevulapradeep.reddy@perficient.com>
2025-11-06 17:13:33 +0530

    Made change in webTables

--- tests/elements/webTables.spec.ts
+++ tests/elements/webTables.spec.ts
@@ -8,20 +8,6 @@ import { handleError } from '../../utils/errorHandler';
 
 test.describe('Web Tables Tests', () => {
 
-  // Global afterEach for screenshots on failure
-  test.afterEach(async ({ page }, testInfo) => {
-    if (testInfo.status !== testInfo.expectedStatus) {
-      if (!fs.existsSync('screenshots')) {
-        fs.mkdirSync('screenshots');
-      }
-      const safeTitle = testInfo.title.replace(/[^a-zA-Z0-9-_]/g, '_');
-      const screenshotPath = path.join('screenshots', `${safeTitle}.png`);
-      await page.screenshot({ path: screenshotPath, fullPage: true });
-      console.error(` Test failed: ${testInfo.error?.message}`);
-      console.log(` Screenshot saved at: ${screenshotPath}`);
-    }
-  });
-
   test('Add, edit, and delete records in Web Tables', async ({ page }) => {
     try {
       const webTablesPage = new WebTablesPage(page);
@@ -70,7 +56,7 @@ test.describe('Web Tables Tests', () => {
 
     }
     catch (err) {
-      await handleError('Radio Button Test', err);
+      await handleError('Radio Button Test', err,page);
     }
   });
 });
